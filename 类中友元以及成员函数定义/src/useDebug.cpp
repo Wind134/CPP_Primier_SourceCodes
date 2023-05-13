821ea522d02c6dc7f3e134267703db7355234406
Ping <li283254184@gmail.com>
2023-05-13 22:48:44 +0800

    Recorrect the commits

--- 类中友元以及成员函数定义/src/useDebug.cpp
+++ 类中友元以及成员函数定义/src/useDebug.cpp
@@ -15,16 +15,16 @@ int main()
 
 	IO_Subsystem ioErrs;        // by default, don't print any debugging
 	// no debugging here
-	if (ioErrs.default_debug()) // if (false || debug.any())
+	if (ioErrs.default_debug()) // if (true && debug.any())
 		cerr << "print message 3" << endl;
 		
 	ioErrs.set_debug(true);     // turn on debugging
-	if (ioErrs.default_debug()) // if (false || debug.any())
+	if (ioErrs.default_debug()) // if (true && debug.any())
 		cerr << "print message 4" << endl;
 	ioErrs.set_debug(false);    // okay, debugging section complete
 
 	HW_Subsystem hw;
-	hw.set_debug(true);
-	if (ioErrs.default_debug() || hw.default_debug()) // if (false || debug.any())
+	hw.set_debug(true);			// 打开硬件调试
+	if (ioErrs.default_debug() || hw.default_debug()) // if ((true && false) || (true && true))
 		cerr << "print message 5" << endl;
 }
